7e059108f52d2e96edaea14a45ff995288807eec
Jessada63070023 <73677830+Jessada63070023@users.noreply.github.com>
2020-11-19 20:07:50 +0700

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,25 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'jessada63070023.mysql.database.azure.com', 'Jessada63070023@jessada63070023', 'EFCjgd73', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+$link = $_POST['link'];
+
+
+$sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
+
+
+if (mysqli_query($conn, $sql)) {
+    header("Location: index.php");
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
